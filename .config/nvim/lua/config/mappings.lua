312b0fa08eb5a512f6f06bc0e532acb7aa61afc1
ratioguy <176092341+Ratioguy@users.noreply.github.com>
2025-06-22 12:09:47 -0700

    remove nvim confs

--- .config/nvim/lua/config/mappings.lua
+++
@@ -1,9 +0,0 @@
--- My bindings
-local map = vim.keymap.set
-
--- Ctrl + hjkl to move arrows in insert mode
-map("i", "<C-h>", "<Left>", { desc = "move left" })
-map("i", "<C-l>", "<Right>", { desc = "move right" })
-map("i", "<C-j>", "<Down>", { desc = "move down" })
-map("i", "<C-k>", "<Up>", { desc = "move up" })
-
