ab02749532fd9489129b46aa3ae7a13ef7cb0133
ratioguy <176092341+Ratioguy@users.noreply.github.com>
2025-06-22 19:48:18 -0700

    readd nvim configs

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,27 @@
+-- My neovim options
+
+-- Set colourscheme to gruvbox dark
+vim.o.background = "dark"
+vim.cmd([[colorscheme gruvbox]])
+
+-- Make mouse copy from terminal
+-- vim.cmd('set mouse=c')
+vim.o.mouse = "c"
+-- Use system keyboard
+vim.o.clipboard = "unnamedplus"
+
+-- Show and highlight line number
+vim.o.number = true
+vim.o.cursorline = true
+vim.o.cursorlineopt = "number"
+
+-- Autocomplete common actions
+vim.cmd('inoremap { {}<Esc>ha')
+vim.cmd('inoremap ( ()<Esc>ha')
+vim.cmd('inoremap [ []<Esc>ha')
+vim.cmd('inoremap " ""<Esc>ha')
+vim.cmd("inoremap ' ''<Esc>ha")
+vim.cmd('inoremap ` ``<Esc>ha')
+
+-- Case insenitive searching
+vim.o.ignorecase = true 
