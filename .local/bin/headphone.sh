4cd0e23fe2e25c679acab88bfbb2d0974d7dab5c
basedcode <176092341+ratioguy@users.noreply.github.com>
2025-07-20 12:29:58 -0700

    update headphone and speaker scripts to have slight delay when sending notification

--- .local/bin/headphone.sh
+++ .local/bin/headphone.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pactl set-default-sink alsa_output.pci-0000_00_1f.3.analog-stereo && notify-send -t 8000 Output: Headphones
+pactl set-default-sink alsa_output.pci-0000_00_1f.3.analog-stereo ; sleep 0.2 ; notify-send -t 8000 Output: Headphones
--- .local/bin/speaker.sh
+++ .local/bin/speaker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pactl set-default-sink alsa_output.usb-HP_HP_Speaker_Bar-00.analog-stereo && notify-send -t 8000 Output: Speaker
+pactl set-default-sink alsa_output.usb-HP_HP_Speaker_Bar-00.analog-stereo ; sleep 0.2 ; notify-send -t 8000 Output: Speaker
