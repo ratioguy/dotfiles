68c2307bd99d0964972b3965d30671745be2d14f
basedcode <176092341+ratioguy@users.noreply.github.com>
2025-07-27 10:59:53 -0700

    allow bar.sh to show multiple batteries

--- .local/bin/bar.sh
+++ .local/bin/bar.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 status () { 
-	echo -n " $(acpi | awk '{print $4}' 2>&1 | sed -n '1p') | $(date '+%B %d, %Y %I:%M %p')"
+	echo -n " $(acpi | awk '{print $4}' 2>&1) | $(date '+%B %d, %Y %I:%M %p')"
 }
 
 while :; do
