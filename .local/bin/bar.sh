74191d31c77e36511fee51ce936ac2d715c13ffe
basedcode <176092341+ratioguy@users.noreply.github.com>
2025-07-26 23:15:04 -0700

    revert last commit

--- .local/bin/bar.sh
+++ .local/bin/bar.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 status () { 
-	echo -n " $(acpi | awk '{print $4}' 2>&1 | awk '6' | sed '$ s/,$//g') | $(date '+%B %d, %Y %I:%M %p')"
+	echo -n " $(acpi | awk '{print $4}' 2>&1 | sed -n '1p') | $(date '+%B %d, %Y %I:%M %p')"
 }
 
 while :; do
