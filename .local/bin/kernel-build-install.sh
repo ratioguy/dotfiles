2ffb78652a00a96505f8555a655fe9900403b76b
ratioguy <176092341+ratioguy@users.noreply.github.com>
2025-10-20 18:24:21 -0700

    add kernel build script

---
+++ .local/bin/kernel-build-install.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Run this script as the superuser.
+
+# This script is meant for Arch Linux, please modify to your systems needs, also this script is POSIX complient so no need for bash.
+
+# To see how fast the kernel compiles run this script with the prefix time.
+
+# Welcome message.
+echo "Welcome, to the linux kernal compile script.\n"
+
+# Make (can specify threads instead of nproc).
+make -s -j$(( $(nproc) * 2 )) march=x86_64 
+
+# Make kernel modules.
+make modules
+
+# Install kernel modules.
+make modules_install
+
+# Make compressed bzimage of kernel.
+make bzimage
+
+# Copies the kernel into the /boot directory.
+cp -v arch/x86/boot/bzImage /boot/vmlinuz-linux-based
+
+mkinitcpio -k linux-based -g /boot/initramfs-linux-based.img
+
+cp System.map /boot/System.map-linux-based
+ln -sf /boot/System.map-linux-based /boot/System.map
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# End of compile message.
+echo "\nCompile and install completed."
