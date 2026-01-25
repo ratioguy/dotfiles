545dc877aa81449ea49509f3cd55f4a19fc2ae96
ratioguy <176092341+ratioguy@users.noreply.github.com>
2026-01-24 16:13:27 -0800

    update yt-downloader script

--- .local/bin/yt-downloader.sh
+++ .local/bin/yt-downloader.sh
@@ -2,4 +2,4 @@
 
 echo "Enter the Download Link:"
 read link
-yt-dlp $link -P ~/Downloads/Youtube/ --merge-output-format mp4 -f 'bestvideo[height<=?720]+bestaudio/best' --sponsorblock-remove sponsor,intro,outro,interaction,selfpromo,preview --exec 'setsid -f mpv --no-terminal $link'
+yt-dlp $link -P ~/Downloads/Youtube/ --merge-output-format mp4 -f 'bestvideo[height<=?720]+bestaudio/best' --sponsorblock-remove sponsor,intro,outro,interaction,selfpromo,preview -o '%(title)s.%(ext)s' --exec 'setsid -f mpv --no-terminal $link'
