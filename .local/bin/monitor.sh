f38ab6483d559c0495aa7684c2d0e1686ef5f1fd
ratioguy <176092341+ratioguy@users.noreply.github.com>
2026-02-02 17:37:16 -0800

    update configs, remove newsboat config, add newsboat config and diction

--- .local/bin/monitor.sh
+++ .local/bin/monitor.sh
@@ -4,7 +4,12 @@ IN=$(xrandr | grep "eDP" | grep " connected" | sed -e "s/\([A-Z0-9]\+\) connecte
 EXT=$(xrandr | grep "DP-2" | grep " connected" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
 
 if (xrandr | grep "$EXT disconnected"); then
-    xrandr --output $EXT --off --output $IN --auto
+	xrandr --output $EXT --off --output $IN --auto &
+	xset r rate 250 50 &
+	xwallpaper --zoom ~/Pictures/wallpapers/White-Mountain.jpg &
 else
-    xrandr --output $IN --off --output $EXT --auto
+	xrandr --output $IN --off --output $EXT --auto
+	xset r rate 250 50 &
+	xwallpaper --zoom ~/Pictures/wallpapers/White-Mountain.jpg &
+
 fi
