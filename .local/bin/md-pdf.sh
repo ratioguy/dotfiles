315a6db5bb5d5a0ce4bfcec9454d50923fedffac
ratioguy <176092341+ratioguy@users.noreply.github.com>
2026-03-07 12:40:20 -0800

    alot of stuff

--- .local/bin/md-pdf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo 'What markdown file do you want to convert:'
-read file
-echo 'What filename should the output have:'
-read name
-pandoc --pdf-engine=pdfroff --toc-depth=1 $file -o $name.pdf
