55671a04e1cf499ff2f4c15ed5c8c5340ef8efec
ratioguy <176092341+ratioguy@users.noreply.github.com>
2026-08-04 12:34:34 -0700

    update autostart gammastep

--- .local/bin/autostart.sh
+++ .local/bin/autostart.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-gammastep -c /home/jim/.config/redshift.conf &
+gammastep -c /home/jim/.config/gammastep/gammastep.conf &
 syncthing --no-browser &>/dev/null
 dunst &>/dev/null
 playerctld &>/dev/null
 wl-paste --watch cliphist store &>/dev/null
+wl-clip-persist --clipboard regular
 swaybg -m fill -i ~/Pictures/wallpapers/White-Mountain.jpg &>/dev/null
+killall easyeffects &
 
 # Screensharing
 export XDG_CURRENT_DESKTOP=sway
