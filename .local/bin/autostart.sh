c372235751949b3b6d9e6fefb428d0ae1983cf40
ratioguy <176092341+ratioguy@users.noreply.github.com>
2026-09-06 15:33:48 -0700

    update sh items

--- .local/bin/autostart.sh
+++ .local/bin/autostart.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 gammastep -c /home/jim/.config/gammastep/gammastep.conf &
-# syncthing --no-browser &>/dev/null
+syncthing --no-browser &>/dev/null
 playerctld &>/dev/null
 mpd &>/dev/null
 wl-paste --watch cliphist store &>/dev/null
 wl-clip-persist --clipboard regular &
 swaybg -m fill -i ~/Pictures/wallpapers/rock.jxl &>/dev/null
+foot --server &
 killall easyeffects &
 
 # Screensharing
